b19db82267ad35e719f8dd963b613eb768001f55
verina-mourad <verinamourad02@gmail.com>
2026-05-29 14:45:42 +0300

    up8

--- src/app/(main)/Shop/page.tsx
+++ src/app/(main)/Shop/page.tsx
@@ -2,7 +2,7 @@ import { getCategories } from '@src/services/getcategories'
 import { getProducts } from '@src/services/getproduct'
 import ProductClient from '@src/app/_component/ProductClient'
 import getBrands from '@src/services/getBrands'
-
+export const dynamic = "force-dynamic";
 const Page = async () => {
   const products = await getProducts()
   const categories = await getCategories()
