faa71257a26ebbb463ced9eafdf37e1a493979a7
verina-mourad <verinamourad02@gmail.com>
2026-05-29 14:23:36 +0300

    up4

--- src/app/_component/Menubar.tsx
+++ src/app/_component/Menubar.tsx
@@ -1,23 +1,36 @@
 'use client'
+
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Menubar = () => {
-    const pathname=usePathname()
-    const profiles=[
-        {path:'/ProfileInfo',title:'Profile Info'},
-        {path:'/Addresses',title:'Addresses'},
-        {path:'/Password',title:'Password'},
-        {path:'/Logout',title:'Logout'},
-    ]
-  return <>
-  <nav className='flex gap-4'>
-  {profiles.map((profile)=>(
-    <Link href={profile.path}className={pathname === profile.path ? "text-blue-500 font-bold" : ""} >{profile.title}</Link>
-  ))
+  const pathname = usePathname()
+
+  const profiles = [
+    { path: '/ProfileInfo', title: 'Profile Info' },
+    { path: '/Addresses', title: 'Addresses' },
+    { path: '/Password', title: 'Password' },
+    { path: '/Logout', title: 'Logout' },
+  ]
+
+  return (
+    <nav className="flex gap-4">
+      {profiles.map((profile) => (
+        <Link
+          key={profile.path}
+          href={profile.path}
+          className={
+            pathname === profile.path
+              ? "text-blue-500 font-bold"
+              : ""
+          }
+        >
+          {profile.title}
+        </Link>
+      ))}
+    </nav>
+  )
 }
-  </nav>
-  </>
 
 export default Menubar
